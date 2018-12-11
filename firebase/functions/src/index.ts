16f95f6dd6f338acb2d04a75d47c6919c937ae0f
nagaminenot <nagamine@slogan.jp>
2018-12-12 07:26:54 +0900

    firebaseの反応を正しくした

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -4,14 +4,20 @@ import * as functions from "firebase-functions";
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  console.log("request");
-  console.log(request.body);
+  let payload = JSON.parse(request.body.payload);
+  let is_add_task = payload.actions[0].value;
+  let user_name = payload.user.name;
+  let text =
+    is_add_task == "true"
+      ? `:white_check_mark: ${user_name}が追加しました！`
+      : `:hand: ${user_name}が追加を許可しませんでした。`;
+
   response.send({
     text: "XorListに追加しますか？",
     attachments: [
       {
         attachment_type: "default",
-        text: ":white_check_mark: 追加しました！",
+        text: text,
         color: "#3AA3E3"
       }
     ]
