325b1f1e47466c51180ae27191c3481d326500b4
nagaminenot <nagamine@slogan.jp>
2018-12-17 08:18:15 +0900

    追加はできるがレスポンスが帰って来ない

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,16 +1,28 @@
 import * as functions from "firebase-functions";
 
+const admin = require('firebase-admin');
+admin.initializeApp()
+const db = admin.firestore()
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest(async (request, response) => {
   let payload = JSON.parse(request.body.payload);
-  console.log(payload);
   let request_value = JSON.parse(payload.actions[0].value);
   let user_name = payload.user.name;
   let text = request_value.is_add
     ? `:white_check_mark: ${user_name}が追加しました！`
     : `:hand: ${user_name}が追加を許可しませんでした。`;
+
+  if(request_value.is_add) {
+    await db.collection('tasks').add({
+      "title": `${request_value.text}`,
+      "description": `${request_value.link.permalink}`,
+      "id": 3,
+      "sequence": 2
+    })
+  }
 
   response.send({
     text: `XorListに追加しますか？\n${request_value.link.permalink}`,
